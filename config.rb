aac17c87853c96715ca972dd91a88cf0faf28a33
Guillermo Rangel <guillermorangel@Guillermos-MacBook-Pro.local>
2017-05-04 02:48:38 -0400

    update

--- config.rb
+++ config.rb
@@ -43,3 +43,10 @@ page '/*.txt', layout: false
 activate :livereload do |reload|
     reload.no_swf = true
 end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+
+  activate :asset_host, :host => '//hellocandor.com/build/'
+end
